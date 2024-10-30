6e5b9880310d1760919a3712488621b9e9771fb3
CristinaPicazo <cristina.picazo.merlos@gmail.com>
2024-10-30 17:23:55 +0100

    listarGastos y etiquetas

--- js/gestionPresupuesto.js
+++ js/gestionPresupuesto.js
@@ -2,6 +2,8 @@
 
 // TODO: Variable global
 let presupuesto = 0;
+let gastos = [];
+let idGasto = 0;
 
 function actualizarPresupuesto(nuevoPresupuesto) {
   if (isNaN(nuevoPresupuesto) || nuevoPresupuesto < 0) {
@@ -21,8 +23,12 @@ function mostrarPresupuesto() {
 function CrearGasto(nuevadesc, nuevoValor, nuevaFecha, ...NuevasEtiquetas) {
   this.valor = nuevoValor >= 0 ? nuevoValor : 0;
   this.descripcion = nuevadesc;
-  this.fecha = nuevaFecha;
-  this.etiquetas = NuevasEtiquetas.length > 0 ? [] : [...NuevasEtiquetas];
+
+  if (Date.parse(nuevaFecha)) {
+    this.fecha = Date.parse(nuevaFecha);
+  } else {
+    this.fecha = Date.parse(new Date());
+  }
 
   this.mostrarGasto = function () {
     return `Gasto correspondiente a ${this.descripcion} con valor ${this.valor} €`;
@@ -37,35 +43,52 @@ function CrearGasto(nuevadesc, nuevoValor, nuevaFecha, ...NuevasEtiquetas) {
   };
 
   this.mostrarGastoCompleto = function () {
+    let listarGasto = `Gasto correspondiente a ${this.descripcion} con valor ${
+      this.valor
+    } €.
+Fecha: ${new Date(this.fecha).toLocaleString()}
+Etiquetas:\n`;
 
-  };
-
-  this.actualizarFecha = function (nuevaFecha) {
-
+    for (let etiqueta of this.etiquetas) {
+      listarGasto += `- ${etiqueta}\n`;
     }
-
-  this.anyadirEtiquetas = function (NuevasEtiquetas) {
+    return listarGasto;
   };
 
-  this.borrarEtiquetas = function (...borraEtiqueta) {
- 
+  this.actualizarFecha = function (nuevaFecha) {};
+
+  this.anyadirEtiquetas = function (nuevasEtiquetas) {
+    nuevasEtiquetas.forEach((eti) => {
+      this.etiquetas.push(eti);
+      this.etiquetas = borrarDuplicados(this.etiquetas);
+    });
   };
+
+  this.borrarEtiquetas = function (...borraEtiqueta) {};
+
+  this.etiquetas = nuevasEtiquetas.length > 0 ? [] : [...nuevasEtiquetas];
+  this.anyadirEtiquetas(...nuevasEtiquetas);
+
 }
 
-function listarGastos() {}
+function listarGastos() {
+  return gastos;
+}
 function calcularTotalGastos() {}
 function calcularBalance() {}
 function anyadirGasto() {}
 function borrarGasto() {}
 
-
-
-
 // NO MODIFICAR A PARTIR DE AQUÍ: exportación de funciones y objetos creados para poder ejecutar los tests.
 // Las funciones y objetos deben tener los nombres que se indican en el enunciado
 // Si al obtener el código de una práctica se genera un conflicto, por favor incluye todo el código que aparece aquí debajo
-export { mostrarPresupuesto, actualizarPresupuesto, CrearGasto,  listarGastos,
+export {
+  mostrarPresupuesto,
+  actualizarPresupuesto,
+  CrearGasto,
+  listarGastos,
   anyadirGasto,
   calcularTotalGastos,
   calcularBalance,
-  borrarGasto, };
+  borrarGasto,
+};
