241dc50e471e4fe1f0a4435a765aaf5906ef37e9
CristinaPicazo <cristina.picazo.merlos@gmail.com>
2024-12-03 09:33:22 +0100

    presupuesto

--- js/generarDatosEstaticos.js
+++ js/generarDatosEstaticos.js
@@ -1,2 +1,6 @@
 import * as gestionPreWeb from "./gestionPresupuestoWeb.js";
 import * as gestionPre from "./gestionPresupuesto.js";
+
+gestionPre.actualizarPresupuesto(1500);
+let presu = gestionPre.mostrarPresupuesto();
+gestionPreWeb.mostrarDatoEnId("presupuesto", presu);
